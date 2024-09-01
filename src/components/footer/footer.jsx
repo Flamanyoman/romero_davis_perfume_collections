2a158fb56510619fcab3107725229df33931b708
Omizegba <flamanyoman@gmail.com>
2024-09-01 16:23:56 +0100

    done

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,29 +1,44 @@
-import { NavLink } from "react-router-dom";
-import { IoMdHome } from "react-icons/io";
+import { NavLink } from 'react-router-dom';
+import { IoMdHome } from 'react-icons/io';
 // import { MdAccountBalance } from "react-icons/md";
-import { MdAccountBalanceWallet } from "react-icons/md";
-import { CgProfile } from "react-icons/cg";
+import { MdAccountBalanceWallet } from 'react-icons/md';
+import { CgProfile } from 'react-icons/cg';
 // import { FaShoppingCart } from "react-icons/fa";
-import { BiMoneyWithdraw } from "react-icons/bi";
+import { BiMoneyWithdraw } from 'react-icons/bi';
 
 export default function FooterComponent() {
-
-    return (
-        <footer className="bg-[#302aaf] bg_img fixed !top-auto !bottom-0 !left-0 !right-0">
-            <nav className="flex items-center pt-3 justify-evenly">
-                <NavLink to="/" className="text-3xl text-[white] flex flex-col justify-evenly items-center w-fit p-0">
-                    <IoMdHome /><span className="block w-fit text-[.7rem]">Home</span>
-                </NavLink>
-                <NavLink to="/recharge" className="text-3xl text-[white] flex flex-col justify-evenly items-center w-fit p-0">
-                    <MdAccountBalanceWallet /><span className="block w-fit text-[.7rem]">Recharge</span>
-                </NavLink>
-                <NavLink to="/withdraw" className="text-3xl text-[white] flex flex-col justify-evenly items-center w-fit p-0">
-                    <BiMoneyWithdraw /><span className="block w-fit text-[.7rem]">Withdraw</span>
-                </NavLink>
-                <NavLink to="/profile" className="text-3xl text-[white] flex flex-col justify-evenly items-center w-fit p-0">
-                    <CgProfile /><span className="block w-fit text-[.7rem]">Profile</span>
-                </NavLink>
-            </nav>
-        </footer>
-    )
+  return (
+    <footer className='bg-[#302aaf] bg_img fixed !top-auto !bottom-0 !left-0 !right-0'>
+      <nav className='flex items-center pt-3 justify-evenly'>
+        <NavLink
+          to='/'
+          className='text-3xl text-[white] flex flex-col justify-evenly items-center w-fit p-0'
+        >
+          <IoMdHome />
+          <span className='block w-fit text-[.7rem]'>Home</span>
+        </NavLink>
+        <NavLink
+          to='/recharge'
+          className='text-3xl text-[white] flex flex-col justify-evenly items-center w-fit p-0'
+        >
+          <MdAccountBalanceWallet />
+          <span className='block w-fit text-[.7rem]'>Recharge</span>
+        </NavLink>
+        <NavLink
+          to='/withdraw'
+          className='text-3xl text-[white] flex flex-col justify-evenly items-center w-fit p-0'
+        >
+          <BiMoneyWithdraw />
+          <span className='block w-fit text-[.7rem]'>Withdraw</span>
+        </NavLink>
+        <NavLink
+          to='/profile'
+          className='text-3xl text-[white] flex flex-col justify-evenly items-center w-fit p-0'
+        >
+          <CgProfile />
+          <span className='block w-fit text-[.7rem]'>Profile</span>
+        </NavLink>
+      </nav>
+    </footer>
+  );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,51 +1,76 @@
-import HeaderComponent from '../components/header/header'
-import FooterComponent from '../components/footer/footer'
-import ProductsComponents from '../components/products/items'
-import stars from '/imgs/stars_rating_black.svg'
-import safesecure from '/imgs/safesecure.svg'
-import trusted from '/imgs/trusted.svg'
-import fastfreeshipping from '/imgs/fastfreeshipping.svg'
+import HeaderComponent from '../components/header/header';
+import FooterComponent from '../components/footer/footer';
+import ProductsComponents from '../components/products/items';
+import stars from '/imgs/stars_rating_black.svg';
+import safesecure from '/imgs/safesecure.svg';
+import trusted from '/imgs/trusted.svg';
+import fastfreeshipping from '/imgs/fastfreeshipping.svg';
 
 export default function Homepage() {
+  return (
+    <section className='h-screen bg-slate-400'>
+      <HeaderComponent />
+      <main className='w-full'>
+        <p className='flex items-center justify-center w-full p-3 bg-white'>
+          <img src={stars} alt={stars} className='mr-3' /> OVER 20 MILLION
+          CUSTOMERS
+        </p>
 
-    return (
-        <section className="h-screen bg-slate-400">
-            <HeaderComponent />
-            <main className="w-full">
-                <p className='flex items-center justify-center w-full p-3 bg-white'><img src={stars} alt={stars} className="mr-3" /> OVER 20 MILLION CUSTOMERS</p>
-                <div className="banner h-[70vh] flex flex-col justify-start align-center pt-10">
-                    <h1 className="mx-auto font-sans text-3xl font-bold w-fit">Shop Summer Deals</h1>
-                    <p className="w-3/4 mx-auto mt-6 _text-gray-600 text-[1.1rem]">At FragranceX.com, our mission is to provide you with the largest selection of perfumes and colognes at the lowest prices.</p>
-                </div>
-                <div className='w-full my-8 bg-white'>
-                    <button className='block container bg-[#302aaf] bg_img text-white text-lg font-bold py-4 rounded'>SHOP ALL PERFUMES </button>
-                    <div className='w-full bg-[#302aaf] bg_img'>
-                        <marquee className='block w-3/4 mx-auto mt-8 px-12  text-white text-lg font-bold py-4'>Free Shipping Over $35</marquee>
-                    </div>
-                </div>
-                <div className='flex items-center justify-between bg-[#f5f5f5] py-8'>
-                    <div className='flex flex-col items-center justify-between'>
-                        <img src={safesecure} alt={safesecure}
-                            className='block w-[50px] aspect-auto' />
-                        <p className='font-[500] text-black w-3/4 mx-auto mt-2 text-center'>Safe & Secure
-                            Checkout</p>
-                    </div>
-                    <div className='flex flex-col items-center justify-between'>
-                        <img src={trusted} alt={trusted}
-                            className='block w-[50px] aspect-auto' />
-                        <p className='font-[500] text-black w-3/4 mx-auto mt-2 text-center'>100% Authentic
-                            Fragrances</p>
-                    </div>
-                    <div className='flex flex-col items-center justify-between'>
-                        <img src={fastfreeshipping} alt={fastfreeshipping}
-                            className='block w-[50px] aspect-auto' />
-                        <p className='font-[500] text-black w-3/4 mx-auto mt-2 text-center'>Items Ship
-                            Same Day</p>
-                    </div>
-                </div>
-                <ProductsComponents />
-            </main>
-            <FooterComponent />
-        </section>
-    );
+        <div className='banner h-[50vh]'>
+          <div className='w-full h-full bg-black bg-opacity-25 flex flex-col justify-center items-center'>
+            <h1 className='font-sans text-3xl font-bold'>Shop Summer Deals</h1>
+            <p className='w-3/4 mt-6 text-white text-[1.1rem]'>
+              At FragranceX.com, our mission is to provide you with the largest
+              selection of perfumes and colognes at the lowest prices.
+            </p>
+          </div>
+        </div>
+
+        <div className='w-full my-8 bg-white'>
+          <button className='block container bg-[#302aaf] bg_img text-white text-lg font-bold py-4 rounded'>
+            SHOP ALL PERFUMES{' '}
+          </button>
+          <div className='w-full bg-[#302aaf] bg_img'>
+            <marquee className='block w-3/4 mx-auto mt-8 px-12  text-white text-lg font-bold py-4'>
+              Free Shipping Over $35
+            </marquee>
+          </div>
+        </div>
+        <div className='flex items-center justify-between bg-[#f5f5f5] py-8'>
+          <div className='flex flex-col items-center justify-between'>
+            <img
+              src={safesecure}
+              alt={safesecure}
+              className='block w-[50px] aspect-auto'
+            />
+            <p className='font-[500] text-black w-3/4 mx-auto mt-2 text-center'>
+              Safe & Secure Checkout
+            </p>
+          </div>
+          <div className='flex flex-col items-center justify-between'>
+            <img
+              src={trusted}
+              alt={trusted}
+              className='block w-[50px] aspect-auto'
+            />
+            <p className='font-[500] text-black w-3/4 mx-auto mt-2 text-center'>
+              100% Authentic Fragrances
+            </p>
+          </div>
+          <div className='flex flex-col items-center justify-between'>
+            <img
+              src={fastfreeshipping}
+              alt={fastfreeshipping}
+              className='block w-[50px] aspect-auto'
+            />
+            <p className='font-[500] text-black w-3/4 mx-auto mt-2 text-center'>
+              Items Ship Same Day
+            </p>
+          </div>
+        </div>
+        <ProductsComponents />
+      </main>
+      <FooterComponent />
+    </section>
+  );
 }
